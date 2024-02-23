dea59ab60e26adf5bfbdcb6253de321e3bae78f1
Réthey-Prikkel Krisztián <147428349+rpkrisz@users.noreply.github.com>
2024-02-23 10:55:17 +0100

    Add javaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+
+
+body.append("main");
+const main = body.querySelector("main");
+
+main.appendChild("h1")
+const h1 = main.querySelector("h1");
+
+h1.innerText = "Hello";
