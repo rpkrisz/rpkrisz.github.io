6da1cf4536e170e369504f621fa079bdf1820eca
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-25 21:05:33 +0200

    Set assets

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,16 @@ function App() {
         <a href="https://www.linkedin.com/in/rethey-prikkel-krisztian/" target="_blank">
           My linkedin profile
         </a>
-        <div id="logger">
-          <span id="toggel">
-            <img src="/assets/sun_icon.png" alt="light" />
-          </span>
-        </div>
       </header>
       <main>
         <h1>Poject links</h1>
         <ul className="flex justify-center">
           <li>
-            <img src="assets/burgonya_minta.png" alt="Egy kép a játékról" />
+            <img src="../public/assets/burgonya_minta.png" alt="Egy kép a játékról" />
             <a href="https://rpkrisz.github.io/JS-BurgonyaTerkep/">Burgonya térkép</a>
           </li>
           <li>
-            <img src="assets/berkalk_minta.png" alt="Egy kép a alkalmazásról" />
+            <img src="../public/assets/berkalk_minta.png" alt="Egy kép a alkalmazásról" />
             <a href="https://rpkrisz.github.io/Berkalkulator/"> Bérkalkulátor</a>
           </li>
         </ul>
