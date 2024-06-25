69967c1b6631bdb95848249106f8366da1b5582f
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-25 20:10:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header>
+        <h1>HI this is my Portfolio</h1>
+        <div id="logger">
+          <span id="toggel">
+            <img src="/assets/sun_icon.png" alt="light" />
+          </span>
+        </div>
+      </header>
+      <main>
+        <a href="https://www.linkedin.com/in/rethey-prikkel-krisztian/" target="_blank">
+          My linkedin profile
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <h1>Poject links</h1>
+        <ul>
+          <li>
+            <img src="assets/burgonya_minta.png" alt="Egy kép a játékról" />
+            <a href="https://rpkrisz.github.io/JS-BurgonyaTerkep/">Burgonya térkép</a>
+          </li>
+          <li>
+            <img src="assets/berkalk_minta.png" alt="Egy kép a alkalmazásról" />
+            <a href="https://rpkrisz.github.io/Berkalkulator/"> Bérkalkulátor</a>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <p>© 2024 - Réthey-Prikkel Kriszsztián</p>
+      </footer>
     </>
   );
 }
