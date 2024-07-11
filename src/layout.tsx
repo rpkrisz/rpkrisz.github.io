1e26d115723c1f807c50b9de12e024b79d81cf99
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-11 13:45:39 +0200

    Create Footer comp

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,6 @@
 import {Outlet} from "react-router-dom";
 import Menu from "./menu";
+import Footer from "./Components/Footer";
 
 function Layout() {
   return (
@@ -10,9 +11,7 @@ function Layout() {
       <main className="felx justify-start flex-grow  m-0 px-8 py-2 relative">
         <Outlet />
       </main>
-      <footer className="bg-gray-800 text-white p-4 flex justify-center items-center">
-        <p>© 2024 - Réthey-Prikkel Kriszsztián</p>
-      </footer>
+      <Footer />
     </>
   );
 }
