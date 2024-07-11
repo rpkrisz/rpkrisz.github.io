91c8fc5b3df8a5b5874b9b4bf3b48bf303853bf8
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-11 13:45:20 +0200

    Import icons

---
+++ src/Components/FawIcons.tsx
@@ -0,0 +1,120 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+  faFilter,
+  faMagnifyingGlass,
+  faEnvelope,
+  faXmark,
+  faUser,
+  faKey,
+  faLocationDot,
+  faBuilding,
+  faHouseLaptop,
+  faHourglassEnd,
+  faHourglass,
+  faGraduationCap,
+  faTrash,
+  faPenToSquare,
+  faEye,
+  faCircleCheck,
+  faCircleXmark,
+  faUserPen,
+  faHourglassStart,
+  faFloppyDisk,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {faGithub, faLinkedin, faSquareFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons";
+
+export function InstagramIcon(params) {
+  return <FontAwesomeIcon icon={faInstagram} {...params} />;
+}
+
+export function GithubIcon(params) {
+  return <FontAwesomeIcon icon={faGithub} {...params} />;
+}
+<FontAwesomeIcon icon={faGithub} />;
+export function SquareFacebookIcon(params) {
+  return <FontAwesomeIcon icon={faSquareFacebook} {...params} />;
+}
+
+export function FloppyDiskIcon(params) {
+  return <FontAwesomeIcon icon={faFloppyDisk} {...params} />;
+}
+
+export function UserPenIcon(params) {
+  return <FontAwesomeIcon icon={faUserPen} {...params} />;
+}
+export function HourglassStartIcon(params) {
+  return <FontAwesomeIcon icon={faHourglassStart} {...params} />;
+}
+
+export function CircleCheckIcon(params) {
+  return <FontAwesomeIcon icon={faCircleCheck} {...params} />;
+}
+
+export function CircleXmarkIcon(params) {
+  return <FontAwesomeIcon icon={faCircleXmark} {...params} />;
+}
+
+export function PenToSquareIcon(params) {
+  return <FontAwesomeIcon icon={faPenToSquare} {...params} />;
+}
+
+export function EyeIcon(params) {
+  return <FontAwesomeIcon icon={faEye} {...params} />;
+}
+
+export function FilterIcon(params) {
+  return <FontAwesomeIcon icon={faFilter} {...params} />;
+}
+
+export function MagnifyingGlassIcon(params) {
+  return <FontAwesomeIcon icon={faMagnifyingGlass} {...params} />;
+}
+
+export function XmarkIcon(params) {
+  return <FontAwesomeIcon icon={faXmark} {...params} />;
+}
+
+export function EnvelopeIcon(params) {
+  return <FontAwesomeIcon icon={faEnvelope} {...params} />;
+}
+
+export function UserIcon(params) {
+  return <FontAwesomeIcon icon={faUser} {...params} />;
+}
+
+export function KeyIcon(params) {
+  return <FontAwesomeIcon icon={faKey} {...params} />;
+}
+
+export function LocationDotIcon(params) {
+  return <FontAwesomeIcon icon={faLocationDot} {...params} />;
+}
+
+export function BuildingIcon(params) {
+  return <FontAwesomeIcon icon={faBuilding} {...params} />;
+}
+
+export function HouseLaptopIcon(params) {
+  return <FontAwesomeIcon icon={faHouseLaptop} {...params} />;
+}
+
+export function HourglassEndIcon(params) {
+  return <FontAwesomeIcon icon={faHourglassEnd} {...params} />;
+}
+
+export function HourglassIcon(params) {
+  return <FontAwesomeIcon icon={faHourglass} {...params} />;
+}
+
+export function GraduationCapIcon(params) {
+  return <FontAwesomeIcon icon={faGraduationCap} {...params} />;
+}
+
+export function TrashIcon(params) {
+  return <FontAwesomeIcon icon={faTrash} {...params} />;
+}
+
+export function LinkedinIcon(params) {
+  return <FontAwesomeIcon icon={faLinkedin} {...params} />;
+}
