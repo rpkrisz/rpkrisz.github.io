091e6f5e83fe828df10c64dd75e705cde65d4410
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-11 13:47:00 +0200

    Update ListElem.tsx

--- src/Components/ListElem.tsx
+++ src/Components/ListElem.tsx
@@ -7,10 +7,25 @@ const ListElem: FC<{link: string; name: string; imageSrc: string; thisPic: strin
   thisPic,
 }) => {
   return (
-    <li className="flex flex-col justify-center max-w-2xl min-w-96 mx-auto">
-      <a href={link} className="flex justify-center flex-grow flex-col p-5 hover:bg-slate-800">
-        <h1>{name}</h1>
-        <img src={imageSrc} alt={`Egy kép ${thisPic}`} className="h-80 m-3 self-center" />
+    <li className="flex flex-col justify-center max-w-screen-2xl min-h-svh  min-w-96 mx-auto">
+      <h1>{name}</h1>
+      <div className="flex flex-wrap">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum labore minima odio error, quo sequi explicabo
+          dolorem deleniti libero nisi voluptas culpa, quis vero suscipit quaerat quasi eos alias deserunt. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Dolorum labore minima odio error, quo sequi explicabo dolorem
+          deleniti libero nisi voluptas culpa, quis vero suscipit quaerat quasi eos alias deserunt.
+        </p>
+      </div>
+      <a href={link} className="flex justify-center flex-grow flex-col p-3 hover:bg-primary-content hover:rounded-lg">
+        <div className="mockup-browser bg-base-300 border">
+          <div className="mockup-browser-toolbar">
+            <div className="input">{link}</div>
+          </div>
+          <div className="bg-base-200 flex justify-center ">
+            <img src={imageSrc} alt={`Egy kép ${thisPic}`} />
+          </div>
+        </div>
       </a>
     </li>
   );
