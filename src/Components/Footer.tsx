e4c350fbe392822c25ad1cdfd519fb7559e4a1db
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-17 16:35:50 +0200

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,11 @@
-import {LinkedinIcon, EnvelopeIcon, SquareFacebookIcon, GithubIcon, InstagramIcon} from "./FawIcons";
+import {LinkedinIcon, EnvelopeIcon, GithubIcon} from "./FawIcons";
 
 export default function Footer() {
   return (
     <footer className="footer text-neutral-content items-center p-4 bg-gray-800">
       <aside className="grid-flow-col items-center">
-        <img src="./assets/horizontal-logo.png" alt="Logo" className="size-16 inline" />
-      <p className="text-center">Copyright © {new Date().getFullYear()} - Réthey-Prikkel Kriszsztián</p>
+        <img src="/assets/horizontal-logo.png" alt="Logo" className="size-16 inline" />
+        <p className="text-center">Copyright © {new Date().getFullYear()} - Réthey-Prikkel Kriszsztián</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <a href="mailto:rp.krisz123@gmail.com" target="_blank">
