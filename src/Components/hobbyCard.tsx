9fb9647ad122faf6c2fb08033f8d8aeeffec8568
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-29 23:41:58 +0200

    Adjust size

--- src/Components/hobbyCard.tsx
+++ src/Components/hobbyCard.tsx
@@ -3,14 +3,14 @@ import H3 from "./H3";
 
 const HobbyCard: FC<{title: string; children?: string; imageSrc: string}> = ({title, children, imageSrc}) => {
   return (
-    <div className="flex flex-col justify-center max-w-2xl min-w-40 mx-auto bg-primary p-2 rounded-md size-80">
+    <div className="flex flex-col justify-center max-w-2xl min-w-40 mx-auto bg-primary p-2 rounded-md">
       {children ? (
         <img src={imageSrc} alt={"Image about the hobby."} className="h-32 m-3 self-center" />
       ) : (
-        <img src={imageSrc} alt={"Image about the hobby."} className="h-60 m-3 self-center" />
+        <img src={imageSrc} alt={"Image about the hobby."} className="h-44 m-3 self-center" />
       )}
       <H3 reverse={true}>{title}</H3>
-      <p className="text-secondary-content">{children}</p>
+      <p className="text-secondary-content text-justify p-2">{children}</p>
     </div>
   );
 };
