3d46c94e6351c10d9619ff337e0a4ec572c199a2
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-07-30 11:24:29 +0200

    Make description conditional

--- src/Components/hobbyCard.tsx
+++ src/Components/hobbyCard.tsx
@@ -10,7 +10,7 @@ const HobbyCard: FC<{title: string; children?: string; imageSrc: string}> = ({ti
         <img src={imageSrc} alt={"Image about the hobby."} className="h-44 m-3 self-center" />
       )}
       <H3 reverse={true}>{title}</H3>
-      <p className="text-secondary-content text-justify p-2">{children}</p>
+      {children && <p className="text-secondary-content text-justify p-2">{children}</p>}
     </div>
   );
 };
